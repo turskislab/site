e74d759c45b9fbc6ba99f77ffec99eaba455f359
Artur Tamborski <tamborskiartur@gmail.com>
2022-03-18 22:24:32 +0100

    a

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ if [ -d "public" ]; then
   echo "push frontend"
   aws s3 sync                                \
     --delete public                          \
-    "s3://codeforpoznan-public/${RESOURCE}"  \
+    "s3://codeforpoznan-public/${AWS_RESOURCE_NAME}"  \
 
 
   echo "refresh CDN"
@@ -38,27 +38,27 @@ fi
 if [ -f "lambda.zip" ]; then
 
   echo "upload lambdas"
-  aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${RESOURCE}_serverless_api.zip"
-  aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${RESOURCE}_migration.zip"
+  aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${AWS_RESOURCE_NAME}_serverless_api.zip"
+  aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${AWS_RESOURCE_NAME}_migration.zip"
 
 
   echo "refresh lambdas"
   aws lambda update-function-code                           \
     --s3-bucket     "codeforpoznan-lambdas"                 \
-    --s3-key        "${RESOURCE}_serverless_api.zip"        \
-    --function-name "${RESOURCE}_serverless_api"            \
+    --s3-key        "${AWS_RESOURCE_NAME}_serverless_api.zip"        \
+    --function-name "${AWS_RESOURCE_NAME}_serverless_api"            \
   | jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
 
   aws lambda update-function-code                           \
     --s3-bucket     "codeforpoznan-lambdas"                 \
-    --s3-key        "${RESOURCE}_migration.zip"             \
-    --function-name "${RESOURCE}_migration"                 \
+    --s3-key        "${AWS_RESOURCE_NAME}_migration.zip"             \
+    --function-name "${AWS_RESOURCE_NAME}_migration"                 \
   | jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
 
 
   echo "run migrations"
   aws lambda invoke                         \
-    --function-name "${RESOURCE}_migration" \
+    --function-name "${uuuRESOURCE}_migration" \
     response.json                           \
   > request.json                            \
 
